f0c9216d7909ee25507b3b825bb71fc7e615a61e
Danil <daniil.ibragimov.10@mail.ru>
2025-10-08 14:00:14 +0300

    in process

---
+++ practic3.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Movie {
+  int year;
+  std::string name;
+  double rating;
+public:
+ Movie (int y, std::string n, double r) {
+  year = y;
+  name = n;
+  rating = r;
+ }
+ void Print() {
+  std::cout << year << name << rating << std::endl;
+ }
+};
+
+
+void InputInfo(std::vector<Movie>&  vec) {
+  std::string name;
+  int year;
+  double rating;
+
+  std::cout << "Введите название: " << std::endl;
+  std::cin >> name;
+  std::cout << "Введите год выпуска: " << std::endl;
+  std::cin >> year;
+  std::cout << "Введите рейтинг: " << std::endl;
+  std::cin >> rating;
+
+  Movie film(year,name,rating);
+  vec.push_back(i)
+}
+
+
+int main() {
+  std::vector<Movie> storage;
+  std:;cout << "Введите количесвто фильмов: " << std::endl;
+
+  
+  for ()
+}
